cd10ddc33af5590795beaf26d21789dba68aeb6b
UC-ConSERT <consert.uc@gmail.com>
2022-07-13 09:49:25 +1200

    Adding templates to GBS directory

---
+++ 5_Intro_to_GBSdata/STACKS_denovo/1_ustacks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#########################################################################################################
+# Running the ustacks pipeline for STACKSv2.41. Values of M ranging from 1 - 8 were tested while values
+# of m ranging from 3-7 were tested for each value of M. 
+#########################################################################################################
+work=/data/Tbulleri/trimmed_cropped_radtags2/ # Set working directory
+out=/data/Tbulleri/ustacks/ # Set output directory
+count=1 # Initialise the counter. Ustacks requires unique integers as inputs for -i
+for M in {1..8} # Range of M
+do
+    for j in {3..7} # Range of m
+    do
+        mkdir ${out}ustacksM${M}m${j}
+        for fq in ${work}*fq.gz
+        do
+            base=$(basename ${fq} .fq.gz)
+            printf "\nRunning Ustacks for ${base}...\n"
+            ustacks -f ${fq} -i ${count} -o ${out}ustacksM${M}m${j}/ -M ${M} -m $j -p 30 -d
+            interval=$((count + 1))
+        done &
+        count=1
+    done
+done
